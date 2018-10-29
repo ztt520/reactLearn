52c04cdb138072688692dda9f0e1fc599b401d69
zoutingting <437482411@qq.com>
2018-10-29 14:54:06 +0800

    pc首页导航的搭建

--- src/js/root.js
+++ src/js/root.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
+import { Button } from 'antd';
+import PCIndex from './components/pc_index';
+import 'antd/dist/antd.css';
+import MediaQuery from 'react-responsive';
 
 export default class Root extends React.Component{
   render(){
     return (
-    <div>Init</div>
+      <div>
+        {/* <MediaQuery query='(min-device-width:1224px)'> */}
+          <PCIndex/>
+        {/* </MediaQuery> */}
+        {/* <MediaQuery query='(min-device-width:1224px)'>
+          <PCIndex/>
+        </MediaQuery> */}
+    </div>
     );
   };
 }
